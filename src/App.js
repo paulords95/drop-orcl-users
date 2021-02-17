481f31f6766c3595c374421ccd5e6a89c40c2405
paulords95 <paulords95@gmail.com>
2021-02-17 13:54:06 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import TextField from "@material-ui/core/TextField";
+
+import Button from "@material-ui/core/Button";
 
 function App() {
+  const [text, setText] = useState("");
+  const [error, setError] = useState({
+    show: false,
+    message: "",
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrap">
+      <div className="container">
+        <h1 className="title">
+          Liberar conexão bloqueada no banco de dados do sistema
+        </h1>
+        <div className="text-wrap">
+          <TextField
+            id="outlined-number"
+            className="input"
+            aria-label="primary"
+            error={error.show}
+            helperText={error.message}
+            label="Usuário"
+            type="text"
+            onChange={(value) => {
+              if (value.length > 1) {
+                setError({
+                  show: false,
+                  message: "",
+                });
+              }
+              setText(value.target.value);
+            }}
+            InputLabelProps={{
+              shrink: true,
+              style: {
+                color: "white",
+                borderRadius: 15,
+              },
+            }}
+            InputProps={{
+              style: {
+                color: "white",
+                textAlign: "center",
+              },
+            }}
+            variant="outlined"
+            color="primary"
+          />
+          <form className="btn" method="POST">
+            <Button
+              onClick={(e) => {
+                e.preventDefault();
+                if (text.length < 1) {
+                  setError({
+                    show: true,
+                    message: "Informe um usuário",
+                  });
+                }
+                if (text.length > 1) {
+                  setError({
+                    show: false,
+                    message: "",
+                  });
+                }
+                console.log("Enviar");
+              }}
+              variant="contained"
+              color="primary"
+              type="submit"
+            >
+              Liberar
+            </Button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
