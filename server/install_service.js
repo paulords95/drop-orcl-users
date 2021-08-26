1428614f29386dcf150c5f076773ed0a070b5366
paulords95 <paulords95@gmail.com>
2021-08-26 09:29:51 -0300

    fix api url

---
+++ server/install_service.js
@@ -0,0 +1,15 @@
+﻿const Service = require("node-windows").Service;
+const svc = new Service({
+  name: "Server - Derrubar Conexão ERP",
+  description: "Server - Derrubar Conexão ERP",
+  script: "C:\\localapps\\blockedusers\\server\\index.js",
+  env: {
+    value: process.env.USER,
+  },
+});
+
+svc.on("install", function () {
+  svc.start();
+  console.log("Serviço instalado");
+});
+svc.install();
---
+++ server/uninstall_service.js
@@ -0,0 +1,10 @@
+﻿var Service = require("node-windows").Service;
+var svc = new Service({
+  name: "Server - Derrubar Conexão ERP",
+  description: "Server - Derrubar Conexão ERP",
+  script: "C:\\localapps\\blockedusers\\server\\index.js",
+});
+svc.on("uninstall", function () {
+  console.log("Uninstall complete.");
+});
+svc.uninstall();
